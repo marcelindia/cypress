2d4877b97d0a6d41ba5a5c9f9243c82f5fc13991
Diana Marcelin <marcelindiana@yahoo.com>
2022-10-05 18:21:37 -0400

    first commit

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,29 @@
+// describe('My First Test', () => {
+//   it('Does not do much!', () => {
+//     expect(true).to.equal(false)
+//   })
+// })
+
+//--Visiting A Page
+// describe('My First Test', () => {
+//   it('Visits the Kitchen Sink', () => {
+//     cy.visit('https://example.cypress.io')
+//   })
+// })
+//--Querying an Element
+// describe('My First Test', () => {
+//   it('finds the content "type"', () => {
+//     cy.visit('https://example.cypress.io')
+
+//     cy.contains('type')
+//   })
+// })
+
+//--Clicking An Element
+// describe('My First Test', () => {
+//   it('clicks the link "type"', () => {
+//     cy.visit('https://example.cypress.io')
+
+//     cy.contains('type').click()
+//   })
+// })
